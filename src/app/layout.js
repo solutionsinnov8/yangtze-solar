3461a6a1285dd448d095b92157c0a6403c29a62d
Zeeshan <mzeeshi187@gmail.com>
2025-06-23 15:31:00 +0500

    some auth and contact us changings

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,6 +21,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="icon" href="/favicon.png" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
